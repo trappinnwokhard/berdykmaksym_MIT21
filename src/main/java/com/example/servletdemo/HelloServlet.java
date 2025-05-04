7be0a0683783a7c5d1aa1ee0488cb563b8ea48e4
trappinnwokhard <maksimberdik0@gmail.com>
2025-05-04 13:52:54 +0300

    tomecat 10

--- src/main/java/com/example/servletdemo/HelloServlet.java
+++ src/main/java/com/example/servletdemo/HelloServlet.java
@@ -8,10 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().write("<h1>Hello from HelloServlet!</h1>");
     }
 }
+
